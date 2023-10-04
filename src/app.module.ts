a175ceea95bd985ebb394f7542661f9d329098c3
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-10-04 21:55:51 +0300

    Add users CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -10,6 +11,7 @@ import configuration from './config/configuration';
       load: [configuration],
       isGlobal: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],

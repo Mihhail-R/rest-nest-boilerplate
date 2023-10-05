49bf19280b6a5ab06814fbe882323fb988a6e50f
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-10-05 15:43:26 +0300

    Add some e2e tests, need to split databases eventually not to create conflicts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import * as session from 'express-session';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,13 +13,36 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.use(
+      session({
+        secret: 'secret',
+        resave: false,
+        cookie: {
+          maxAge: 1000 * 60 * 60, // 1 hour
+        },
+      }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/auth/register', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/auth/register')
+      .send({
+        email: 'test@test.test',
+        password: 'test',
+        name: 'Test',
+      })
+      .expect(201);
+  });
+
+  it('/auth/login (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: 'test@test.test',
+        password: 'test',
+      })
+      .expect(201);
   });
 });

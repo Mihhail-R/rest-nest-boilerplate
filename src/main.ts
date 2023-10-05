dfa99e0b687c236a0f6bf817e1ce541a96eaa6c0
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-10-05 15:42:32 +0300

    Initialize necessary express middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const secret = process.env.COOKIE_SECRET || 'secret';
+  app.use(
+    session({
+      secret,
+      resave: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60, // 1 hour
+      },
+      saveUninitialized: false,
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();

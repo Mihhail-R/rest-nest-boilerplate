ef13ea5cf496df37356de5e6b02da5b8209808ae
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-10-06 16:19:22 +0300

    Use interceptors to exclude password fields from user responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Delete,
   UseGuards,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -15,18 +17,21 @@ import { AuthenticatedGuard } from '../auth/authenticated.guard';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get()
   @UseGuards(AuthenticatedGuard)
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
   @UseGuards(AuthenticatedGuard)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Patch(':id')
   @UseGuards(AuthenticatedGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {

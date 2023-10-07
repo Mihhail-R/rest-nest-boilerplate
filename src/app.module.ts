9b95bfb94decbddb0e6475264e62b9b5078fb116
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-10-07 20:38:40 +0300

    Inject throttler into app module and initialize global guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { configuration } from './config/configuration';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -13,8 +15,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-op
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(configuration().database as TypeOrmModuleOptions),
+    ThrottlerModule.forRoot(configuration().throttler),
     UsersModule,
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}

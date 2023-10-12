f7a186fa542602fc69724d162cff923c48fe897b
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-10-12 22:52:41 +0300

    Initialize CacheModule with redis as storage method of choice

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { configuration } from './config/configuration';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import type { RedisClientOptions } from 'redis';
 
 @Module({
   imports: [
@@ -16,6 +19,12 @@ import { APP_GUARD } from '@nestjs/core';
     }),
     TypeOrmModule.forRoot(configuration().database as TypeOrmModuleOptions),
     ThrottlerModule.forRoot(configuration().throttler),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      host: configuration().redis.host,
+      port: configuration().redis.port,
+      password: configuration().redis.password,
+    }),
     UsersModule,
     AuthModule,
   ],
@@ -23,6 +32,10 @@ import { APP_GUARD } from '@nestjs/core';
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
     },
   ],
 })

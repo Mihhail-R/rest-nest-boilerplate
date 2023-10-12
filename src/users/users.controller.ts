6af84d434c2f3fdf23adaad81147a6a495cae6dd
Mihhail Rehokainen <mihhail.rehokainen@egrupp.ee>
2023-10-12 22:52:55 +0300

    Add cache-control headers to get operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   UseGuards,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Header,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -20,6 +21,10 @@ export class UsersController {
   @UseInterceptors(ClassSerializerInterceptor)
   @Get()
   @UseGuards(AuthenticatedGuard)
+  @Header(
+    'Cache-Control',
+    'max-age=60, stale-while-revalidate=120, stale-if-error=240, public',
+  )
   findAll() {
     return this.usersService.findAll();
   }
@@ -27,6 +32,10 @@ export class UsersController {
   @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
   @UseGuards(AuthenticatedGuard)
+  @Header(
+    'Cache-Control',
+    'max-age=60, stale-while-revalidate=120, stale-if-error=240, public',
+  )
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
